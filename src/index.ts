6fc4e668aeeb067e9e55f915039dee91559afc30
Kamal Joshi <kkjoshi.it@gmail.com>
2022-05-30 07:39:30 +0530

    feat: add basic recursive working search code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+
 type FuzzySearchFSProps = {
   caseSensitive?: boolean;
 };
-export const fuzzySearchFS = (
-  path: string,
+export const indexPath = (path: string, indexStoragePath: string): void => {
+  return;
+};
+export const fuzzySearchFS = async (
+  rootPath: string,
   searchText: string,
   options?: FuzzySearchFSProps
-): string[] => {
-  console.log(path, searchText, options);
-  return [];
+): Promise<string[]> => {
+  if (!options) {
+    options = {};
+  }
+  const results = [];
+  const stats = await fsPromises.stat(rootPath);
+  if (stats.isFile()) {
+    const fileContent = await fsPromises.readFile(rootPath, 'utf8');
+    if (options.caseSensitive) {
+      searchText = searchText.toLowerCase();
+    }
+    if (fileContent.includes(searchText)) {
+      results.push(rootPath);
+    }
+  } else {
+    console.log(rootPath, searchText, options);
+    const paths = await fsPromises.readdir(rootPath);
+    console.log('Paths:', paths);
+    for (let i = 0; i < paths.length; i++) {
+      const returnedResults = await fuzzySearchFS(
+        path.join(rootPath, paths[i]),
+        searchText,
+        options
+      );
+      returnedResults.forEach(r => results.push(r));
+    }
+  }
+  return results;
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -8,22 +8,18 @@ describe('index', () => {
       mock({
         'path/to/fake/dir': {
           'some-file.txt': 'file content here',
-          'empty-dir': {
-            /** empty directory */
-          },
+          'empty-dir': {},
         },
         'path/to/some.png': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
-        'some/other/path': {
-          /** another empty directory */
-        },
+        'some/other/path': {},
       });
     });
-    it('should return a single file', function () {
-      const results = fuzzySearchFS('path/', 'file');
+    it('should return a single file', async function () {
+      const results = await fuzzySearchFS('path/', 'file');
       expect(results).toStrictEqual(['path/to/fake/dir/some-file.txt']);
     });
-    it('should return empty if does not match', function () {
-      const results = fuzzySearchFS('path/', 'randomText');
+    it('should return empty if does not match', async function () {
+      const results = await fuzzySearchFS('path/', 'randomText');
       expect(results).toStrictEqual([]);
     });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
